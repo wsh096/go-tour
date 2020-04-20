1f9aa807c7a21b7e5e63a726b0612b320911211f
wsh096 <wsh096@gmail.com>
2020-04-20 14:36:19 +0000

    go tour 12 변수의 초기화 타입 써주면 결정 안 써주면 뒤의 값에 따라 결정

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
-func split(sum int) (x,y int) {
-  x= sum *4/9
-  y= sum -x
-  return 
+var x, y,z = 1, 2, 3.14  //int=1, 2, 3 이렇게 입력해도 되고 안 입력해도 됨! 다 int가 아닐 수도 있으니까 안 입력하면 메모리 많이 사용
+var c, python, java = true, false, "No!"
+//x=1여기선 변경 x
+func main() {
+  //x =1 여기서 각 변수의 값을 바꿔 줄 수 있다
+  fmt.Println(x, y,z,c, python, java)
 }
 
-func main() {
-  fmt.Println(split(17))
-}
+//int defalut 0
+//bool(true/false) defalut 0
+
+///이와 같이 바로 옆에서도 가능하며 bool의 경우 특정 t/f string은 ""사용!
