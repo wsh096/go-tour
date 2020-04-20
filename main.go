601d904803662d8f5918ca6ab2329765d6b5a12c
wsh096 <wsh096@gmail.com>
2020-04-20 14:12:10 +0000

    go tour_함수 1

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import "fmt"
-import "math"
-//이렇게 따로 쓸 수도 있고 import ("fmt" "math")
+
+func add(x int, y int) int {
+  return x + y
+}
 
 func main() {
-  fmt.Printf("Now you have %g problems.",math.Nextafter(2,3))
+  fmt.Println(add(42,13))
 }
