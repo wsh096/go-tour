69d371f36c56c5aad4fb465c7a28391e063609c6
wsh096 <wsh096@gmail.com>
2020-04-21 13:54:11 +0000

    go tour_26 구조체 접근법

--- main.go
+++ main.go
@@ -8,5 +8,7 @@ type Vertex struct {//X,Y int}로 써도 됨
   }
 
 func main() {
-  fmt.Println(Vertex{1,2})
+  v := Vertex{1,2}
+  v.X =4//.을 이용해서 X의 값에 접근해서 변경할 수도 있고 그걸 확인 할 수도 있음! 신기하구만!
+  fmt.Println(v.X)
 }
