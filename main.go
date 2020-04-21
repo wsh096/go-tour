34fc147059dab7520175fc5fb6162698e562d338
wsh096 <wsh096@gmail.com>
2020-04-21 13:09:35 +0000

    go tour_20 if의 사용 소괄호x 중괄호 필수! sqrt는 제곱근 찾는 거!

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
+import(
+  "fmt"
+  "math"
+)
 
-
-func main(){
-  
-  for  {
-    
+func sqrt(x float64) string{
+  if x<0{
+    return sqrt(-x) + "i"
   }
-  
+  return fmt.Sprint(math.Sqrt(x))
+}
+func main() {
+  fmt.Println(sqrt(2),sqrt(-4))
 }
 
+//sprt는 제곱근을 뜻하는 함수이고
+//i는 허수를 뜻함 우와...엄청 나네...
