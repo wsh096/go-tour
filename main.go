884ae6c55887c0c3c6b1a0ac6b665e8806c39001
wsh096 <wsh096@gmail.com>
2020-04-21 13:04:43 +0000

    go tour_19 for 무한 루프 조건문 없으면 무한루프 만들 수 있다! 이 때는 import "fmt"도 없음 뭐에 쓰는 건진 모르겠음

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
-import"fmt"
+
 
 func main(){
-  s:= 1
-  for s<1000 {
-    s += s
+  
+  for  {
+    
   }
-  fmt.Println(s)
   
 }
 
-//Go에서 "While" 사용하기
-//이전의 예제에서 처럼 조건문만 표시하면 //C언어에서 while 을 사용하듯 for 를 사용할 수 있습니다.
-
-//변수를 선언하는 const방법
-//상수는 const 키워드와 함께 변수처럼 선언합니다.
-//상수는 문자(character), 문자열(string), 부울(boolean), 숫자 타입 중의 하나가 될 수 있습니다.
