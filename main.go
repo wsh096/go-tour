e0b17c5cb4746da90f76206ad393cfa155d2b0a1
wsh096 <wsh096@gmail.com>
2020-04-21 13:26:37 +0000

    go tour_22 if와 else

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import(
 func pow(x,n,lim float64) float64 {
   if v := math.Pow(x,n); v<lim {
     return v
+  }else{//위의 조을 else도 그대로 받음!
+  //즉, v := math.Pow(x,n); v<lim이외의 조건 v>=lim이 자동으로 입력
+    fmt.Printf("%g >= %g\n",v,lim)
   }
   return lim
 }
@@ -19,4 +22,7 @@ func main() {
 //pow는 3,2면 3을 두 번 제곱한 값을 뜻하고
 //3,3이면 3을 세 번 제곱함!
 //sprt는 제곱근을 뜻하는 함수이고
-//i는 허수를 뜻함 우와...엄청 나네...
+
+//if 에서 짧은 명령문을 통해 선언된 변수는 else 블럭 안에서도 사용할 수 있습니다.
+
+//%g를 하면 플롯을 적당한 정수 값으로 표현해줌
