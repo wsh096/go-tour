a4303d2612c08c2913a1b7860c2e47a5ac9e86fd
wsh096 <wsh096@gmail.com>
2020-04-21 12:58:53 +0000

    go tour 16_for반복문 식선언/반복 횟수/변환식을 위한 별도의 소괄호가 필요하지 않음!
    쓰지 말라!//
    뒤에 중괄호는 필요!

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import"fmt"
 
-const (
-  Big = 1<<100//2진수의 1을 100자리 오른쪽으로 이동
-  Small = Big>>99//big을 99자리 땡겨옴
-  //2진수상 0001이 0010이므로 2임
-)//const값을 위와 같이 범위로 정의 가능
-
-func needInt(x int) int{ 
-  return x*10+1
- }
-func needFloat(x float64) float64 {
-  return x*0.1
-}
-
 func main(){
-  fmt.Println(needInt(Small))
-  fmt.Println(needFloat(Small))
-  fmt.Println(needFloat(Big))
+  s:= 0
+  for i:=0; i<10; i++{
+    s += i
+  }
+  fmt.Println(s)
+  
 }
 
 //변수를 선언하는 const방법
