444be3a3ef3d616e6070bd148c47291d8a6f1187
wsh096 <wsh096@gmail.com>
2020-04-21 12:45:07 +0000

    go tour14 _const 상수로 변수 변언 사용방법

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
-   //int=1, 2, 3 이렇게 입력해도 되고 안 입력해도 됨! 다 int가 아닐 수도 있으니까 안 입력하면 메모리 많이 사용
+import"fmt"
 
-//x=1여기선 변경 x
-func main() {
-  x, y,z := 1, 2, 3.14
-  var c, python, java = true, false, "No!"
-  //x =1 여기서 각 변수의 값을 바꿔 줄 수 있다
-  fmt.Println(x, y,z,c, python, java)
+const Pi = 3.14
+
+func main(){
+  const W = "안녕"
+  fmt.Println("Hello", W)
+  fmt.Println("Happy",Pi,"Day")
+
+  const T = true
+  fmt.Println("Gp rules?", T)
 }
 
-//int defalut 0
-//bool(true/false) defalut 0
-
-///이와 같이 바로 옆에서도 가능하며 bool의 경우 특정 t/f string은 ""사용!
-
-변수의 짧은 선언인 :=는 함수 밖에서는 불가능!
-즉 함수 내에서 사용하는 특정 용법이다!
-하지만 var x... type = ...는 모두 사용 가능!
+//변수를 선언하는 const방법
+//상수는 const 키워드와 함께 변수처럼 선언합니다.
+//상수는 문자(character), 문자열(string), 부울(boolean), 숫자 타입 중의 하나가 될 수 있습니다.
