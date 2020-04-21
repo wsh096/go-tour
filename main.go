bf129b481457b621e30237273254e100975daa58
wsh096 <wsh096@gmail.com>
2020-04-21 12:56:20 +0000

    go tour15_숫자형 상수

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import"fmt"
 
-const Pi = 3.14
+const (
+  Big = 1<<100//2진수의 1을 100자리 오른쪽으로 이동
+  Small = Big>>99//big을 99자리 땡겨옴
+  //2진수상 0001이 0010이므로 2임
+)//const값을 위와 같이 범위로 정의 가능
+
+func needInt(x int) int{ 
+  return x*10+1
+ }
+func needFloat(x float64) float64 {
+  return x*0.1
+}
 
 func main(){
-  const W = "안녕"
-  fmt.Println("Hello", W)
-  fmt.Println("Happy",Pi,"Day")
-
-  const T = true
-  fmt.Println("Gp rules?", T)
+  fmt.Println(needInt(Small))
+  fmt.Println(needFloat(Small))
+  fmt.Println(needFloat(Big))
 }
 
 //변수를 선언하는 const방법
