a1efb9aa589169bb3ecc1fc60f4795139824e651
wsh096 <wsh096@gmail.com>
2020-04-21 13:16:48 +0000

    go tour_21 for의 경우에도 조건문을 사용할 수 있음 변수의 선언;변수의 조건{} !! pow(x,n)은 x를 n제곱하라는 뜻!

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import(
   "math"
 )
 
-func sqrt(x float64) string{
-  if x<0{
-    return sqrt(-x) + "i"
+func pow(x,n,lim float64) float64 {
+  if v := math.Pow(x,n); v<lim {
+    return v
   }
-  return fmt.Sprint(math.Sqrt(x))
-}
-func main() {
-  fmt.Println(sqrt(2),sqrt(-4))
+  return lim
 }
 
+func main() {
+  fmt.Println(
+    pow(3,2,10), pow(3,3,20))
+}
+//pow는 3,2면 3을 두 번 제곱한 값을 뜻하고
+//3,3이면 3을 세 번 제곱함!
 //sprt는 제곱근을 뜻하는 함수이고
 //i는 허수를 뜻함 우와...엄청 나네...
