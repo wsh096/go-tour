a02f0c40a40984ea9c49ab212bd8865f012b9b90
wsh096 <wsh096@gmail.com>
2020-04-22 07:44:53 +0000

    go tour_32 슬라이스의 길이와 용량은 차이가 있고 배열을 만드는데 make를 할 수 있다.

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main(){
-  p:= [] int {2,3,5,7,11,13}
-  fmt.Println("p ==",p)
-  fmt.Println("p[1:4] ==",p[1:4])
-  fmt.Println("p[:3] ==",p[:3])
-  fmt.Println("p[4:] ==",p[4:])  
+  a:= make([]int,5)//5개의 빈 배열 만듬(길이도 5 용량도 5)
+  printSlice("a",a)
+  b:= make([]int,0,5)
+  printSlice("b",b)//길이는 0, 용량은 5인 배열을 만듬
+  c:=b[:2]
+  printSlice("c",c)//?길이는 2인데//용량은 5
+  d:=b[2:5] //길이3,용량3
+  printSlice("d",d)
 }
-
+func printSlice(s string, x[ ]int) {
+  fmt.Printf("%s len=%d cap=%d %v\n",s,len(x),cap(x),x)
+}
+  
 //슬라이스 자르기
