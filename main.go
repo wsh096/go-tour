c5d1c005ffcc0856cdb15e133bae4490ee91e96c
wsh096 <wsh096@gmail.com>
2020-04-22 09:39:04 +0000

    go_tour_39_m/L(2) _맨 앞이 타입명이면 생략 가능

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ type Vertex struct {
 }
 
 var m = map[string] Vertex{
-  "Bell Labs": Vertex{
+  "Bell Labs":{
     40.68433, -74.39967,
     },
-  "Google": Vertex{
+  "Google": {
     37.42202,-122.08408,
     },
   }
@@ -21,3 +21,6 @@ func main() {
   }
 //맵의 리터럴
 //맵의 문자열
+
+//가장 상위의 타입이 타입명이라면 문자열의 타입명 생략이 가능
+//Vertex생략가능
