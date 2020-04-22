db238c3d5f599fa1f4b642608e8ddb2ff800775f
wsh096 <wsh096@gmail.com>
2020-04-22 10:17:21 +0000

    go_tour_Switch_45_switch := case ~ :

--- main.go
+++ main.go
@@ -1,31 +1,23 @@
 package main
 
-import "fmt"
+import( "fmt"
+        "runtime"
+)
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-    fn0 := 0
-
-    fn1 := 0
-
-    return func() int{
-      fn2:=fn1 +fn0
-      fn1, fn0 = fn2, fn1
-      if fn2 == 0 {
-
-            fn1 = 1
-      }
-      return fn2
-    }
-}
 
 func main(){
-  f := fibonacci()
-  for i := 0; i <10; i++ {
-    fmt.Println(f())
+  
+    fmt.Println("Go runs on ")
+    switch os := runtime.GOOS; os {
+      case "darwin" :
+      fmt.Println("OS X.")
+      case "linux":
+      fmt.Println("Linux")
+      default:
+      fmt.Printf("%s",os)
+    }
   }
-}
+
 //그리고 함수는 클로져(full closures) 입니다.
 
 //코드에서 adder 함수는 클로져(closure)를 반환합니다.
