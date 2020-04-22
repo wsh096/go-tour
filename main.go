10edbff5012ac30e95bad9bc384922a1dc438ab3
wsh096 <wsh096@gmail.com>
2020-04-22 09:01:22 +0000

    go tour_33 빈 슬라이스는 nil이고 0,0이다

--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main
 import "fmt"
 
 func main(){
-  a:= make([]int,5)//5개의 빈 배열 만듬(길이도 5 용량도 5)
-  printSlice("a",a)
-  b:= make([]int,0,5)
-  printSlice("b",b)//길이는 0, 용량은 5인 배열을 만듬
-  c:=b[:2]
-  printSlice("c",c)//?길이는 2인데//용량은 5
-  d:=b[2:5] //길이3,용량3
-  printSlice("d",d)
+var z [] int
+fmt.Println(z, len(z),cap(z))
+if z== nil {
+  fmt.Println("nil!")
 }
-func printSlice(s string, x[ ]int) {
-  fmt.Printf("%s len=%d cap=%d %v\n",s,len(x),cap(x),x)
 }
-  
-//슬라이스 자르기
+
+//빈 슬라이스의 기본 값은 nil인 0,0
