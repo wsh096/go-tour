accdfb4191287963fa82bf07e208509e0a0ce40a
wsh096 <wsh096@gmail.com>
2020-04-22 09:45:26 +0000

    go tour_40 맵 항목 수정과 삭제/존재여부 확인

--- main.go
+++ main.go
@@ -2,25 +2,36 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-  Lat, Long float64
-}
-
-var m = map[string] Vertex{
-  "Bell Labs":{
-    40.68433, -74.39967,
-    },
-  "Google": {
-    37.42202,-122.08408,
-    },
-  }
-
 func main() {
-  
+  m := make(map[string]int)
+
+  m["Answer"] = 42
+  fmt.Println("The value:", m["Answer"])
+
+  m["Answer"] =48//수정하기!
+  fmt.Println("The value:", m["Answer"])
+
+  delete(m, "Answer")
+  fmt.Println("The value:", m["Answer"]) //요소 지우기
+
+  v, ok := m["Answer"]
+  fmt.Println("The value:", v, "Present?", ok) //존재여부
+
+
+
   fmt.Println(m)
   }
-//맵의 리터럴
-//맵의 문자열
 
-//가장 상위의 타입이 타입명이라면 문자열의 타입명 생략이 가능
-//Vertex생략가능
+//맵 m 의 요소를 삽입하거나 수정하기:
+
+//m[key] = elem
+//요소 값 가져오기:
+
+//elem = m[key]
+//요소 지우기:
+
+//delete(m, key)
+//키의 존재 여부 확인하기:
+
+//elem, ok = m[key]
+//위의 ok 의 값은 m 에 key 가 존재한다면 true 존재하지 않으면 false , elem 은 타입에 따라 0(zero value) 가 됩니다.
