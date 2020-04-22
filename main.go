343fd459251ef546111136d136d4caf9e68326c1
wsh096 <wsh096@gmail.com>
2020-04-22 09:55:23 +0000

    go_tour_42_함수값

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main 
 import ( 
-    "code.google.com/p/go-tour/wc" 
-    "strings" 
-) 
-func WordCount(s string) map[string]int { 
-    m := make(map[string]int) 
-    for _, v := range strings.Fields(s) { 
-        m[v]++ 
-    } 
-    return m 
-} 
-func main() { 
-    wc.Test(WordCount) 
+"fmt"
+"math"
+)
+
+func main(){
+  hypot := func(x,y float64) float64{
+    return math.Sqrt(x*x +y*y)
+  }
+  fmt.Println(hypot(3,4))
 }
-
-
-//맵 m 의 요소를 삽입하거나 수정하기:
-
-//m[key] = elem
-//요소 값 가져오기:
-
-//elem = m[key]
-//요소 지우기:
-
-//delete(m, key)
-//키의 존재 여부 확인하기:
-
-//elem, ok = m[key]
-//위의 ok 의 값은 m 에 key 가 존재한다면 true 존재하지 않으면 false , elem 은 타입에 따라 0(zero value) 가 됩니다.
