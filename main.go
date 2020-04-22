c30261e0151e6870743216b77ab8b115367428ab
wsh096 <wsh096@gmail.com>
2020-04-22 09:10:14 +0000

    go tour_35 range 활용 _를 통해 해당 값을 빈칸으로 사용하기

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-var pow = [] int{1,2,4,8,16,32,64,128}
-
 func main(){
-for i, v:= range pow {
-  fmt.Printf("2**%d = %d\n", i, v)
+  pow := make([]int, 10)
+for i := range pow {
+  pow[i] = 1<< uint(i)
 }
-
+for i,_ := range pow { //_가 있어야 그 값의 빈칸으로 인식한다!!
+  fmt.Printf("%d\n",i)
+}
 }
 
 //슬라이스에서 range를 사용해 슬라이스 나 맵을 왔다 갔다 할 수 있음 ragne '앞', '뒤'
