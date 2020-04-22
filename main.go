7d7c42e4650a668a33e4b6a2b9a1d58e01b1159f
wsh096 <wsh096@gmail.com>
2020-04-22 10:13:00 +0000

    go_tour_44_피보나치_이것보다 좋은 함수 있음 _조금 문제 있음

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
-package main 
-import 
-"fmt"
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+    fn0 := 0
+
+    fn1 := 0
+
+    return func() int{
+      fn2:=fn1 +fn0
+      fn1, fn0 = fn2, fn1
+      if fn2 == 0 {
+
+            fn1 = 1
+      }
+      return fn2
+    }
+}
 
 func main(){
-  pos, neg := adder(), adder()
-  for i := 0; i<10; i++{
-    fmt.Println(
-      pos(i),
-      neg(-2*i),
-      )
+  f := fibonacci()
+  for i := 0; i <10; i++ {
+    fmt.Println(f())
   }
 }
-
-func adder() func(int) int{
-  sum := 0
-  return func(x int) int {
-    sum += x
-    return sum
-  }
-}
-
 //그리고 함수는 클로져(full closures) 입니다.
 
 //코드에서 adder 함수는 클로져(closure)를 반환합니다.
