4ec955463f236749d7568607515364e5293caa80
wsh096 <wsh096@gmail.com>
2020-04-22 09:32:31 +0000

    go tour_37_map 맵은 값에 키를 지정 세 개의 키 ,,,

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
-import "code.google.com/p/go-tour/pic"
+import "fmt"
 
-func Pic(dx, dy int) [][]uint8 {
-    arr := make([][]uint8, dy)    
-    for i := 0; i < dy; i++ {
-        arr[i] = make([]uint8, dx)
-    }   
-    for i := 0; i < dy; i++ {
-        for j := 0; j < dx; j++ {           
-                arr[i][j] = uint8(i^j)
-        }
-    }   
-    return arr
+type Vertex struct {
+  Lat, Long float64
 }
+
+var m map[string] Vertex
 
 func main() {
-    pic.Show(Pic)
-}
+  m = make(map[string]Vertex)
+  m["Bell Labs"] = Vertex{
+    40.68433, -74.39967,
+  }
+  fmt.Println(m["Bell Labs"])
+  }
+//맵은 값에 키를 지정합니다.
+
+//맵은 반드시 사용하기 전에 make 를 명시해야합니다. (주의: new 가 아닙니다)
+
+//make 를 수행하지 않은 nil 에는 값을 할당할 수 없습니다.
