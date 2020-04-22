a4b42bf597c92bd25ecb6b50350f751a0faf84af
wsh096 <wsh096@gmail.com>
2020-04-22 06:24:01 +0000

    go tour_30_슬라이스는 배열의 값을 가리킴

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-  X,Y int
-}
-
 func main(){
-  v:= new(Vertex)
-  fmt.Println(*v)//포인터여서 *안 붙이면 &가 붙어서 나옴!!!
-  v.X, v.Y = 11, 9
-  fmt.Println(*v) 
+  p:= [] int {2,3,5,7,11,13}
+  fmt.Println("p ==",p)
+
+  for i:=0; i <len(p); i++ {
+    fmt.Printf("p[%d] == %d\n", i, p[i])
+  }
 }
 
+//슬라이스는 배열의 값을 가리킵니다(point). 그리고 배열의 길이를 가지고 있습니다.
 
-
-//new(T) 는 모든 필드가 0(zero value) 이 할당된 T 타입의 !포인터를 반환합니다!
+//[]T 는 타입 T 를 가지는 요소의 슬라이스(slice) 입니다.
