8624fac4e92ff1070ce9284cfc22055774df981b
wsh096 <wsh096@gmail.com>
2020-04-22 07:33:34 +0000

    go tour_31 슬라이스 자르기

--- main.go
+++ main.go
@@ -5,12 +5,9 @@ import "fmt"
 func main(){
   p:= [] int {2,3,5,7,11,13}
   fmt.Println("p ==",p)
-
-  for i:=0; i <len(p); i++ {
-    fmt.Printf("p[%d] == %d\n", i, p[i])
-  }
+  fmt.Println("p[1:4] ==",p[1:4])
+  fmt.Println("p[:3] ==",p[:3])
+  fmt.Println("p[4:] ==",p[4:])  
 }
 
-//슬라이스는 배열의 값을 가리킵니다(point). 그리고 배열의 길이를 가지고 있습니다.
-
-//[]T 는 타입 T 를 가지는 요소의 슬라이스(slice) 입니다.
+//슬라이스 자르기
