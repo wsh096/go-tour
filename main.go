4d59aff258229ac8295994fac9cb666a64cc5b4d
wsh096 <wsh096@gmail.com>
2020-04-22 09:27:24 +0000

    go tour_36 현재로서는 이해가 어려움 이게 뭐야 신기해

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import "code.google.com/p/go-tour/pic"
 
-func main(){
-  pow := make([]int, 10)
-for i := range pow {
-  pow[i] = 1<< uint(i)
-}
-for i,_ := range pow { //_가 있어야 그 값의 빈칸으로 인식한다!!
-  fmt.Printf("%d\n",i)
-}
+func Pic(dx, dy int) [][]uint8 {
+    arr := make([][]uint8, dy)    
+    for i := 0; i < dy; i++ {
+        arr[i] = make([]uint8, dx)
+    }   
+    for i := 0; i < dy; i++ {
+        for j := 0; j < dx; j++ {           
+                arr[i][j] = uint8(i^j)
+        }
+    }   
+    return arr
 }
 
-//슬라이스에서 range를 사용해 슬라이스 나 맵을 왔다 갔다 할 수 있음 ragne '앞', '뒤'
+func main() {
+    pic.Show(Pic)
+}
